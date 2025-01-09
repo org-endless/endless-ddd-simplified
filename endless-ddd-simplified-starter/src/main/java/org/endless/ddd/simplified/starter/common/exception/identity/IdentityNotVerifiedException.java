282fe5741e91d4b40fc3d2adeb95c9c8047882c4
Deng Haozhi <haozhi_deng@126.com>
2025-01-09 17:50:10 +0800

    Version 1.0.0-SNAPSHOT By Deng Haozhi 提交代码模版

---
+++ endless-ddd-simplified-starter/src/main/java/org/endless/ddd/simplified/starter/common/exception/identity/IdentityNotVerifiedException.java
@@ -0,0 +1,35 @@
+package org.endless.ddd.simplified.starter.common.exception.identity;
+
+import org.endless.ddd.simplified.starter.common.exception.common.FailedException;
+import org.endless.ddd.simplified.starter.common.handler.result.type.ErrorCode;
+
+/**
+ * UnIdentityVerificationException
+ * <p>
+ * create 2025/01/09 17:42
+ * <p>
+ * update 2025/01/09 17:42
+ *
+ * @author Deng Haozhi
+ * @since 2.0.0
+ */
+public class IdentityNotVerifiedException extends FailedException {
+
+    private static final ErrorCode ERROR_CODE = ErrorCode.IDA0000;
+
+    public IdentityNotVerifiedException() {
+        super(ERROR_CODE);
+    }
+
+    public IdentityNotVerifiedException(String message) {
+        super(ERROR_CODE, message);
+    }
+
+    public IdentityNotVerifiedException(Throwable throwable) {
+        super(ERROR_CODE, throwable);
+    }
+
+    public IdentityNotVerifiedException(String message, Throwable throwable) {
+        super(ERROR_CODE, message, throwable);
+    }
+}
